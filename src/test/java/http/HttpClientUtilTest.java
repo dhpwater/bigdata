e3fe9d2fb26d52813f78204e7e8303a340bee5b5
ryan <ryan@daihaipeng>
2017-08-10 11:41:32 +0800

    增加httpclient 工具方法

---
+++ src/test/java/http/HttpClientUtilTest.java
@@ -0,0 +1,38 @@
+package http;
+
+import org.junit.Test;
+
+import com.ns.http.HttpClientUtil;
+
+public class HttpClientUtilTest {
+
+//	@Test
+	public void testSendHttpGet() {  
+        String responseContent = HttpClientUtil.getInstance()  
+                .sendHttpGet("http://10.67.1.64:8080/bcm/clusterInfo/get/1502330525480");  
+        System.out.println("reponse content:" + responseContent);  
+        
+        
+    }  
+	
+//	@Test
+	public void testSendHttpsGet() {
+		String responseContent = HttpClientUtil.getInstance()
+				 .sendHttpsGet("https://www.baidu.com");  
+		System.out.println("reponse content:" + responseContent);
+	}
+	
+	@Test
+	public void testSendHttpPost(){
+		String param  = HttpClientUtil.getInstance()  
+                .sendHttpGet("http://10.67.1.64:8080/bcm/clusterInfo/get/1502330525480");  
+		System.out.println("param :" + param);
+		
+		String responseContent = HttpClientUtil.getInstance()
+				.sendHttpPost("http://10.67.1.64:8080/bcm/clusterInfo/set/1502330525480" ,param , "application/json");
+		
+		System.out.println("reponse content:" + responseContent);
+	}
+	
+	
+}
